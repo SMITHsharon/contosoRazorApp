6e991cd7b9f09f7ec1060ddf1c1674a2d7d34833
SMITHsharon <sharon.e.smith2@gmail.com>
2018-04-06 00:22:55 -0500

    modifying Student.cs model

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string LastName { get; set; }
+
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
+        
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
